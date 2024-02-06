68c33ef51fe7d6658d545244a22bf62f1ba6a56f
KJ <kjhouser43@gmail.com>
2024-02-06 07:09:41 -0500

    Created Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+
+namespace TodoApp;
+
+public class Program
+{
+    public static void Main(String[] Args)
+    {
+        Console.WriteLine("Hello world!");
+    }
+}
