4c51ea87f112094593a93959678b2f8938e38c19
KJ <kjhouser43@gmail.com>
2024-02-06 20:38:40 -0500

    Create Home page

--- Program.cs
+++ Program.cs
@@ -5,8 +5,31 @@ namespace TodoApp;
 
 public class Program
 {
+    public static String[] Tasks = { };
+
     public static void Main(String[] Args)
     {
-        Console.WriteLine("Hello world!");
+        // Clear the Console
+        Clear();
+
+        WriteLine("Console Todo App");
+        WriteLine("Your Tasks:");
+
+        //TODO: List the user's tasks.
+        if(Tasks.Length > 0)
+        {
+            for (int i = 0; i < Tasks.Length; i++)
+            {
+                string Task = Tasks[i];
+                WriteLine($"  - {Tasks[i]}");
+            }
+        }
+        else
+        {
+            ForegroundColor = ConsoleColor.Green;
+            Write("  Woohoo! You're all done! :D");
+            ResetColor();
+            WriteLine();
+        }
     }
 }
