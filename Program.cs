d4625d0f664e998c70900e95ad3d499806c78715
KJ <kjhouser43@gmail.com>
2024-03-03 20:09:43 -0500

    Refactored bad code + added comments

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ public class Program
             WriteLine("Console Todo App");
             WriteLine("Your Tasks:");
 
-            // If the Task list's length is > 0,
-            // Display the tasks.
+            // Checks if there are tasks in the list
             if(Tasks.Count > 0)
             {
+                // For every task, print it!
                 for (int i = 0; i < Tasks.Count; i++)
                 {
                     string Task = Tasks[i];
@@ -34,44 +34,65 @@ public class Program
             }
             else
             {
+                // Tell the user they are all caught up.
                 ForegroundColor = ConsoleColor.Green;
                 Write("  Woohoo! You're all done! :D");
                 ResetColor();
                 WriteLine();
             }
 
+            // Command line
+            // current commands:
+            //   * add
+            //   * rem
             Write(">> ");
             string? line = ReadLine();
             
             if (line != null)
             {
-                if (line == "add")
+                switch (line)
                 {
-                    Write("Name: ");
-                    string? newTask = ReadLine();
+                    // If the command is "add", create a new task.
+                    case "add":
+                        // Ask the user for the name of the new task.
+                        Write("Name: ");
+                        string? newTask = ReadLine();
 
-                    if (newTask != null)
-                        Tasks.Add(newTask);
-                }
-                if(line == "rem")
-                {
-                    Write("#: ");
-                    string? input = ReadLine();
-
-                    _ = int.TryParse(input, out int num);
-
-                    num--;
-
-                    if (num >= 0 && num < Tasks.Count)
-                    {
-                        List<string> task_copy = new();
-                        foreach(string task in Tasks)
+                        // add the task to the list.
+                        if (newTask != null)
                         {
-                            if (task != Tasks[num]) task_copy.Add(task);
+                            Tasks.Add(newTask);
                         }
+                        break;
+                    // If the command is "rem", Remove a selected task.
+                    case "rem":
+                        // Ask the user for the number to remove.
+                        Write("#: ");
+                        string? input = ReadLine();
 
-                        Tasks = task_copy;
-                    }
+                        // Parse the input to a number.
+                        _ = int.TryParse(input, out int num);
+
+                        // Subtract 1 from the number
+                        // (To match the numbering on the list)
+                        num--;
+
+                        if (num >= 0 && num < Tasks.Count)
+                        {
+                            // Create a new tasks variable.
+                            List<string> newTasks = new();
+
+                            foreach(string task in Tasks)
+                            {
+                                // check if the current task matches the indexed task.
+                                // If it does, don't add it to the new tasks list.
+                                if (task != Tasks[num]) newTasks.Add(task);
+                            }
+
+                            // Set the task list to the new tasks list.
+                            Tasks = newTasks;
+                        }
+                        break;
                 }
             }
         }
