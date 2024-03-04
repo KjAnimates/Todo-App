df19af7643d98ab6f9f65931d4df19c275934e88
KJ <kjhouser43@gmail.com>
2024-03-03 19:58:50 -0500

    Updated display + add 2 new commands "add" and "rem".

--- Program.cs
+++ Program.cs
@@ -5,31 +5,75 @@ namespace TodoApp;
 
 public class Program
 {
-    public static String[] Tasks = { };
+    public static List<string> Tasks = new();
 
     public static void Main(String[] Args)
     {
-        // Clear the Console
-        Clear();
-
-        WriteLine("Console Todo App");
-        WriteLine("Your Tasks:");
-
-        //TODO: List the user's tasks.
-        if(Tasks.Length > 0)
+        while(true)
         {
-            for (int i = 0; i < Tasks.Length; i++)
+            // Clear the Console
+            Clear();
+
+            WriteLine("Console Todo App");
+            WriteLine("Your Tasks:");
+
+            // If the Task list's length is > 0,
+            // Display the tasks.
+            if(Tasks.Count > 0)
             {
-                string Task = Tasks[i];
-                WriteLine($"  - {Tasks[i]}");
+                for (int i = 0; i < Tasks.Count; i++)
+                {
+                    string Task = Tasks[i];
+                    ForegroundColor = ConsoleColor.Blue;
+                    Write($"  [{i + 1}] ");
+
+                    ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine($"{Tasks[i]}");
+                    ResetColor();
+                }
             }
-        }
-        else
-        {
-            ForegroundColor = ConsoleColor.Green;
-            Write("  Woohoo! You're all done! :D");
-            ResetColor();
-            WriteLine();
+            else
+            {
+                ForegroundColor = ConsoleColor.Green;
+                Write("  Woohoo! You're all done! :D");
+                ResetColor();
+                WriteLine();
+            }
+
+            Write(">> ");
+            string? line = ReadLine();
+            
+            if (line != null)
+            {
+                if (line == "add")
+                {
+                    Write("Name: ");
+                    string? newTask = ReadLine();
+
+                    if (newTask != null)
+                        Tasks.Add(newTask);
+                }
+                if(line == "rem")
+                {
+                    Write("#: ");
+                    string? input = ReadLine();
+
+                    _ = int.TryParse(input, out int num);
+
+                    num--;
+
+                    if (num >= 0 && num < Tasks.Count)
+                    {
+                        List<string> task_copy = new();
+                        foreach(string task in Tasks)
+                        {
+                            if (task != Tasks[num]) task_copy.Add(task);
+                        }
+
+                        Tasks = task_copy;
+                    }
+                }
+            }
         }
     }
 }
